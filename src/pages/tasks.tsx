45748fa9b81886d7b095840e65ebcbb2f2b5422b
Jayedjb765 <jayedjb76@gmail.com>
2025-04-08 17:21:00 +0600

    adding tasks

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/modules/tasks/AddTaskModal";
 import TaskCard from "@/modules/tasks/TaskCard";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -8,8 +9,9 @@ export default function Tasks() {
 
   return (
     <div className="mx-auto max-w-7xl  px-5 mt-56">
-      <div className="text-left -mt-50">
+      <div className="flex justify-between items-center -mt-48">
         <h2>Tasks</h2>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
         {tasks.map((task) => (
